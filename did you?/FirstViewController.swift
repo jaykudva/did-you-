4a18c0b32892dfca7fd82aec3e9efec926f6ca33
Jay Kudva <jaykudva@gmail.com>
2020-05-23 23:07:00 -0700

    initial commit - very basic outline to app interface (most framework ported from tabbed template), added functioning text fields

--- did you?/FirstViewController.swift
+++ did you?/FirstViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    var taskList: [String] = ["1", "2", "3"]
+    var str = ""
+    var err = "nothing new"
+    
+    @IBOutlet weak var taskInput: UITextField!
+    
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        taskInput.delegate = self
+        if taskList.count > 0 {
+            loadUp()
+        }
         // Do any additional setup after loading the view.
+    }
+    
+    func loadUp() {
+        for s in taskList {
+            str = str + s + "\n"
+        }
     }
 
 
+    @IBAction func enterTap(_ sender: Any) {
+        taskList.append(taskInput.text ?? "no task")
+        textView.text = str + "\n" + taskInput.text!
+        taskInput.text = ""
+        
+    }
 }
 
+extension FirstViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Did you?
+## By Jay Kudva[https://jaykudva.me]
+A (hopefully) modern take on productivity apps
