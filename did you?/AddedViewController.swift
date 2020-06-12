aef04b1c3931a280956ba5dc2f3bf7ebe6bf8a21
Jay Kudva <jaykudva@gmail.com>
2020-06-12 16:05:22 -0700

    added "notes added" popup and add functionality when enter is pressed

--- did you?/AddedViewController.swift
+++ did you?/AddedViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class AddedViewController: UIViewController {
 
+    @IBOutlet weak var popup: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     override func viewDidAppear(_ animated: Bool) {
+        popup.layer.shadowOpacity = 1
+        popup.layer.shadowOffset = CGSize.zero
+        popup.layer.shadowColor = UIColor.darkGray.cgColor
+        popup.layer.cornerRadius = 8
         _ = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { timer in
             self.dismiss(animated: true)
         }
